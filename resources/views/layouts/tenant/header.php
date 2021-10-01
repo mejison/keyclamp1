a6ff4f11c551feec119db58cfc3d667098de19be
Sudhi Seshachala <sudhi.seshachala@thomsonreuters.com>
2021-10-01 12:29:02 -0500

    sidebar

--- resources/views/layouts/tenant/header.php
+++ resources/views/layouts/tenant/header.php
@@ -1,6 +1,10 @@
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="">
+
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -10,8 +14,37 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <script src="{{ asset('js/dashboard.js') }}" defer></script>
+
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-</head>ss
+
+    <link href="{{ mix('css/dashboard.css') }}" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+
+
+
+
+    <title>Marketing Simplified</title>
+</head>
